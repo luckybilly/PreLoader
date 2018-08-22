7b306ae60763d56b88f6407c786602538587f56a
qiyi <qiyi@kaishiba.com>
2018-08-22 11:39:53 +0800

    delete useless code

--- pre-loader/src/main/java/com/billy/android/preloader/StatusInitialed.java
+++ pre-loader/src/main/java/com/billy/android/preloader/StatusInitialed.java
@@ -1,8 +1,6 @@
 package com.billy.android.preloader;
 
 
-import com.billy.android.preloader.interfaces.DataListener;
-
 /**
  * initial state
  * @author billy.qi
@@ -17,18 +15,6 @@ class StatusInitialed extends StateBase {
     public boolean startLoad() {
         super.startLoad();
         return worker.doStartLoadWork();
-    }
-
-    @Override
-    public boolean listenData() {
-        super.listenData();
-        return worker.doWaitForDataLoaderWork();
-    }
-
-    @Override
-    public boolean listenData(DataListener listener) {
-        super.listenData(listener);
-        return worker.doWaitForDataLoaderWork(listener);
     }
 
     @Override
